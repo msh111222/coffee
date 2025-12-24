6429704261b9ac92f97ca733eaf3ead47aada81c
Your Name <your.email@example.com>
2025-12-24 13:35:52 +0800

    完善字体样式

--- src/main/java/com/example/coffee/config/WebConfig.java
+++ src/main/java/com/example/coffee/config/WebConfig.java
@@ -8,7 +8,9 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
    public void addResourceHandlers(ResourceHandlerRegistry registry) {
       registry.addResourceHandler(new String[]{"/img/**"}).addResourceLocations(new String[]{"file:/www/img/"}).setCachePeriod(86400);
+      registry.addResourceHandler(new String[]{"/fonts/**"}).addResourceLocations(new String[]{"file:/www/fonts/"}).setCachePeriod(86400);
       System.out.println("========== WebConfig 已加载 ==========");
       System.out.println("静态资源映射: /img/** -> file:/www/img/");
+      System.out.println("静态资源映射: /fonts/** -> file:/www/fonts/");
    }
 }
